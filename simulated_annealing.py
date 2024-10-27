fefbe67514a3d7bb107fdad939b9efaaeb1ccf9f
Arthur Oliveira <arthur_mo@hotmail.com>
2024-10-27 03:33:52 -0300

    algoritmo implementado, avaliar geração de vizinhos não aleatória

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,5 +1,7 @@
 import random
 from collections import defaultdict
+from math import exp
+from pprint import pprint
 
 class Item:
     def __init__(self, weight, value, name):
@@ -54,8 +56,7 @@ class State:
                 return item, self.item_list[item]
         return None, 0  # Retorna None e 0 se o item não for encontrado
 
-def create_items():
-    item_names = ["Diamante", "Ouro", "Prata", "Bronze", "Ferro", "Cobre", "Pedra", "Platina", "Aço", "Carvão"]
+def create_items(item_names):
     items_list = []
     for name in item_names:
         random_weight = random.randint(1, 10)
@@ -64,7 +65,7 @@ def create_items():
 
     return items_list
 
-def create_starting_state(knapsack_weight_capacity, items_list):
+def create_random_state():
     random_state = State()
     random_item = random.choice(items_list)
 
@@ -74,14 +75,78 @@ def create_starting_state(knapsack_weight_capacity, items_list):
 
     return random_state
 
-def simulated_annealing():
-    pass
+def calculate_temperature(start_temperature, temperature_function, changing_rate, current_step):
+    if temperature_function == "exponential":
+        T = start_temperature * (changing_rate ** current_step)
+    elif temperature_function == "linear":
+        T = start_temperature - (current_step * changing_rate)
+    else:
+        return None
+    
+    return T
+
+def create_possible_state(current_state):
+    return create_random_state()
+
+def simulated_annealing(starting_state, start_temperature, temperature_function, changing_rate):
+    current_step = 1
+    T = start_temperature
+    current_state = starting_state
+    best_state = starting_state
+    while (T > 0):
+        possible_state = create_possible_state(current_state)
+        delta_value = possible_state.get_total_value() - current_state.get_total_value() 
+        if(delta_value > 0):
+            current_state = possible_state
+            if(possible_state.get_total_value() > best_state.get_total_value()):
+                best_state = possible_state
+        
+        elif random.random() < exp(delta_value / T):
+            current_state = possible_state
+
+
+        T = calculate_temperature(start_temperature, temperature_function, changing_rate, current_step)
+        current_step += 1
+    
+    return current_state, best_state
+
+knapsack_weight_capacity = 300
+item_names = ["Diamante", "Ouro", "Prata", "Bronze", "Ferro", "Cobre", "Pedra", "Platina", "Aço", "Carvão"]
+number_of_items = len(item_names)
+items_list = create_items(item_names)
 
 def main():
-    knapsack_weight_capacity = 300
-    items_list = create_items()
-    starting_state = create_starting_state(knapsack_weight_capacity, items_list)
-    simulated_annealing()
+    for _ in range (1):
+        starting_state = create_random_state()
+        start_temperature = 100
+        temperature_function = "exponential"
+        changing_rate = 0.9
+        final_state, best_state = simulated_annealing(starting_state, start_temperature, temperature_function, changing_rate)
+        best_state_items = best_state.get_item_list()
+        best_state_items_details = [
+            {
+            'name': item.name,
+            'quantity': quantity,
+            'value': item.value,
+            'weight': item.weight,
+            'total_value': item.value * quantity,
+            'total_weight': item.weight * quantity,
+            'value_weight_ratio': item.value/item.weight,
+            }
+            for item, quantity in best_state_items.items()
+        ]
+
+    print("Itens no Estado:")
+    print("-" * 120)
+    print(f"{'Nome':<15} {'Quantidade':<10} {'Valor':<10} {'Peso':<10} {'Valor Total':<15} {'Peso Total':<15} {'Valor/Peso':<15}")
+    print("-" * 120)
+    for item in best_state_items_details:
+        print(f"{item['name']:<15} {item['quantity']:<10} {item['value']:<10} {item['weight']:<10} {item['total_value']:<15} {item['total_weight']:<15} {item['value_weight_ratio']:<15.2f}")
+    # Exibindo o valor total e o peso total do estado
+    print("-" * 120)
+    print(f"{'Valor Total do Estado:':<35} {best_state.get_total_value():<15}")
+    print(f"{'Peso Total do Estado:':<35} {best_state.get_total_weight():<15}")
+
     # Acesso ao primeiro item
     # item_iterator = iter(starting_state.get_item_list())  # Cria um iterador sobre os itens
     # first_item = next(item_iterator)  # Obtém o primeiro item
